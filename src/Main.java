a74445a9c11f56da87fbd1d5f5e897b8c7fe1079
LelandYan <2721599586@qq.com>
2018-11-19 00:14:50 +0800

    stack

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,21 @@ public class Main {
 
     public static void main(String[] args)
     {
-        Array<Integer> arr = new Array<>(20);
-        for (int i = 0; i < 10; i++) {
-            arr.addLast(i);
+        ArrayStack<Integer> stack = new ArrayStack<>();
+        for (int i = 0; i < 5; i++) {
+            stack.push(i);
+            System.out.println(stack);
         }
-        System.out.println(arr);
-        arr.add(1,100);
-        System.out.println(arr);
-        arr.addFirst(-1);
-        System.out.println(arr);
-        arr.remove(2);
-        System.out.println(arr);
-        arr.remvoeElement(2);
-        System.out.println(arr);
+        stack.pop();
+        System.out.println(stack);
+//        System.out.println(arr);
+//        arr.add(1,100);
+//        System.out.println(arr);
+//        arr.addFirst(-1);
+//        System.out.println(arr);
+//        arr.remove(2);
+//        System.out.println(arr);
+//        arr.remvoeElement(2);
+//        System.out.println(arr);
     }
 }
