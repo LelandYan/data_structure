62635058c2b6468c00bd62338475092f820c6928
LelandYan <2721599586@qq.com>
2018-11-22 23:29:54 +0800

    linkedlist

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
 
     public static void main(String[] args)
     {
-        ArrayStack<Integer> stack = new ArrayStack<>();
-        for (int i = 0; i < 5; i++) {
-            stack.push(i);
-            System.out.println(stack);
-        }
-        stack.pop();
-        System.out.println(stack);
+//        ArrayStack<Integer> stack = new ArrayStack<>();
+//        for (int i = 0; i < 5; i++) {
+//            stack.push(i);
+//            System.out.println(stack);
+//        }
+//        stack.pop();
+//        System.out.println(stack);
 //        System.out.println(arr);
 //        arr.add(1,100);
 //        System.out.println(arr);
@@ -18,5 +18,14 @@ public class Main {
 //        System.out.println(arr);
 //        arr.remvoeElement(2);
 //        System.out.println(arr);
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+        linkedList.add(2,66);
+        System.out.println(linkedList);
+        linkedList.remove(2);
+        System.out.println(linkedList);
     }
 }
