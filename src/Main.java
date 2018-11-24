4d1725707b8250618f7eb769d8a9c6ef6b28facc
LelandYan <2721599586@qq.com>
2018-11-25 00:10:04 +0800

    calculate words

--- src/Main.java
+++ src/Main.java
@@ -33,5 +33,11 @@ public class Main {
         ArrayList<String> words1 = new ArrayList<>();
         FileOperation.readFile("pride-and-prejudice.txt",words1);
         System.out.println("Total words: " + words1.size());
+
+        BSTSet<String> set1 = new BSTSet<>();
+        for (String word:words1) {
+            set1.add(word);
+        }
+        System.out.println("Total different words: " + set1.getSize());
     }
 }
