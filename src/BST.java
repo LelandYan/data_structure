6eedba2a17bfd6a4ac9af2533ae63d3331459074
LelandYan <2721599586@qq.com>
2018-11-24 20:20:15 +0800

    preOrder

--- src/BST.java
+++ src/BST.java
@@ -1,9 +1,9 @@
-public class BST<E extends Comparable<E> > {
-    private class Node{
+public class BST<E extends Comparable<E>> {
+    private class Node {
         public E e;
-        public Node left,right;
+        public Node left, right;
 
-        public Node(E e){
+        public Node(E e) {
             this.e = e;
             left = null;
             right = null;
@@ -13,41 +13,87 @@ public class BST<E extends Comparable<E> > {
     private Node root;
     private int size;
 
-    public BST(){
+    public BST() {
         root = null;
         size = 0;
     }
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public void add(E e){
-        if(root == null){
-            root = new Node(e);
-            size++;
-        }else{
-            add(root,e);
-        }
+    public void add(E e) {
+        root = add(root, e);
     }
 
-    private void add(Node node,E e){
-        if(e.equals(node.e))return;
-        else if(e.compareTo(node.e) < 0 && node.left == null){
-            node.left = new Node(e);
+    private Node add(Node node, E e) {
+        if (node == null) {
             size++;
-            return;
-        }else if(e.compareTo(node.e) > 0 && node.right == null){
-            node.right = new Node(e);
-            size++;
-            return;
+            return new Node(e);
         }
-        if(e.compareTo(node.e) < 0)add(node.left,e);
-        else add(node.right,e);
+        if (e.compareTo(node.e) < 0) node.left = add(node.left, e);
+        else if (e.compareTo(node.e) > 0) node.right = add(node.right, e);
+        return node;
     }
 
+    public boolean contains(E e) {
+        return contains(root, e);
+    }
+
+    private boolean contains(Node node, E e) {
+        if (root == null) return false;
+        if (e.compareTo(node.e) == 0) return true;
+        else if (e.compareTo(node.e) > 0) return contains(node.right, e);
+        else return contains(node.left, e);
+    }
+
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if (node == null) return;
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+
+    }
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        generateBSTString(root,0,res);
+        return res.toString();
+    }
+
+    private void generateBSTString(Node node,int depth,StringBuilder res){
+        if(node == null){
+            res.append(generateDepthString(depth) + "null\n");
+            return;
+        }
+        res.append(generateDepthString(depth) + node.e + "\n");
+        generateBSTString(node.left,depth+1,res);
+        generateBSTString(node.right,depth+1,res);
+    }
+
+    private String generateDepthString(int depth){
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            res.append("--");
+        }
+        return res.toString();
+    }
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+        int[] nums = {5,3,6,8,4,2};
+        for (int num:nums) {
+            bst.add(num);
+        }
+        bst.preOrder();
+        System.out.println();
+        System.out.println(bst);
+    }
 }
