4f884d571733f19be480b47242e5730cd78e5b10
LelandYan <2721599586@qq.com>
2018-11-25 23:40:46 +0800

    map and set practice

--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,41 @@
+import org.omg.PortableInterceptor.INACTIVE;
+
 import java.util.List;
 import java.util.Stack;
 import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.ArrayList;
 
 public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int num : nums1) {
+            if(!map.containsKey(num)){
+                map.put(num,1);
+            }else{
+                map.put(num,map.get(num) + 1);
+            }
+        }
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int num : nums2) {
+            if(map.containsKey(num)){
+                arr.add(num);
+                map.put(num,map.get(num) - 1);
+                if(map.get(num)==0)map.remove(num);
+            }
+        }
+        int[] res = new int[arr.size()];
+        for (int i = 0; i < arr.size(); i++) {
+            res[i] = arr.get(i);
+        }
+        return res;
+    }
+
     public int uniqueMorseRepresentations(String[] words) {
-        String[] dict = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        String[] dict = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
         TreeSet<String> set = new TreeSet<>();
         StringBuilder res = new StringBuilder();
-        for (String word:words) {
+        for (String word : words) {
             for (int i = 0; i < word.length(); i++) {
                 res.append(dict[word.charAt(i) - 'a']);
             }
@@ -16,6 +43,7 @@ public class Solution {
         }
         return set.size();
     }
+
     public ListNode removeElements(ListNode head, int val) {
         while (head != null && head.val == val) {
 //            ListNode delNode = head;
