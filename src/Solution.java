1f7460cc2a22e7a3b01e98a02a579b378fe4ccb9
LelandYan <2721599586@qq.com>
2018-11-29 15:59:53 +0800

    leedcode347

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,48 @@
-import org.omg.PortableInterceptor.INACTIVE;
+
 
 import java.util.List;
 import java.util.Stack;
 import java.util.TreeMap;
 import java.util.TreeSet;
-import java.util.ArrayList;
-
+import java.util.LinkedList;
+import java.util.PriorityQueue;
 public class Solution {
+    private class Freq implements Comparable<Freq>{
+        public int e,freq;
+        public Freq(int e,int freq){
+            this.e = e;
+            this.freq = freq;
+        }
+        @Override
+        public int compareTo(Freq another) {
+            return this.freq - another.freq;
+        }
+    }
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        for (int num:nums) {
+            if(map.containsKey(num)){
+                map.put(num,map.get(num)+1);
+            }else{
+                map.put(num,1);
+            }
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+                (a,b) -> map.get(a) - map.get(b)
+        );
+        for (int key:map.keySet()) {
+            if(pq.size() < k)pq.add(key);
+            else if(map.get(key) > map.get(pq.peek())){
+                pq.remove();
+                pq.add(key);
+            }
+        }
+        LinkedList<Integer> res = new LinkedList<>();
+        while (!pq.isEmpty()){
+            res.add(pq.remove());
+        }
+        return res;
+    }
     public int[] intersection(int[] nums1, int[] nums2) {
         TreeMap<Integer, Integer> map = new TreeMap<>();
         for (int num : nums1) {
