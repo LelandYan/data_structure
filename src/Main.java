0546fcb03282ffbc315f5e084abe91272a9402bb
LelandYan <2721599586@qq.com>
2018-12-05 23:55:34 +0800

    quick find

--- src/Main.java
+++ src/Main.java
@@ -41,6 +41,21 @@ public class Main {
         return (endTime - startTime) / 1000000000.0;
     }
 
+    private static  double testUF(UF uf,int m){
+        int size = uf.getSize();
+        Random random = new Random();
+
+        long startTime = System.nanoTime();
+
+        for (int i = 0; i < m; i++) {
+            int a = random.nextInt(size);
+            int b = random.nextInt(size);
+            uf.isConnected(a,b);
+        }
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
     public static void main(String[] args) {
 //        Integer[] nums = {-2, 0, 3, -5, 2, -1};
 //        SegmentTree<Integer> segTree = new SegmentTree<>(nums, (a, b) -> a + b);
