5e72ac7f54541342137da439854c79abe8f7214f
LelandYan <2721599586@qq.com>
2018-12-13 15:52:08 +0800

    avltree

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class AVLTree<K extends Comparable<K>, V>{
+public class AVLTree<K extends Comparable<K>, V> {
 
     private class Node {
         public K key;
@@ -38,41 +38,52 @@ public class AVLTree<K extends Comparable<K>, V>{
         else if (key.compareTo(node.key) > 0) node.right = add(node.right, key, value);
         else node.value = value;
 
-        node.height = 1 + Math.max(getHeight(node.left),getHeight(node.right));
+        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
         int balanceFactor = getBalanceFactor(node);
-        if(Math.abs(balanceFactor) > 1){
-            System.out.println("unbalanced :" + balanceFactor);
-        }
+//        if(Math.abs(balanceFactor) > 1){
+//            System.out.println("unbalanced :" + balanceFactor);
+//        }
 
-        if(balanceFactor > 1 && getBalanceFactor(node.left)>=0 ){
+        if (balanceFactor > 1 && getBalanceFactor(node.left) >= 0) {
             return rightRotate(node);
         }
-        if(balanceFactor < -1 && getBalanceFactor(node.right)<=0 ){
+        if (balanceFactor < -1 && getBalanceFactor(node.right) <= 0) {
+            return leftRotate(node);
+        }
+        if (balanceFactor > 1 && getBalanceFactor(node.left) < 0) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+        if (balanceFactor < -1 && getBalanceFactor(node.right) > 0) {
+            node.right = rightRotate(node.right);
             return leftRotate(node);
         }
         return node;
     }
-    private Node leftRotate(Node y){
+
+    private Node leftRotate(Node y) {
         Node x = y.right;
         Node T2 = x.left;
 
         x.left = y;
         y.right = T2;
 
-        y.height = Math.max(getHeight(y.left),getHeight(y.right));
-        x.height =  Math.max(getHeight(x.left),getHeight(x.right));
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
         return x;
     }
-    private Node rightRotate(Node y){
+
+    private Node rightRotate(Node y) {
         Node x = y.left;
         Node T3 = x.right;
 
         x.right = y;
         y.left = T3;
-        y.height = Math.max(getHeight(y.left),getHeight(y.right));
-        x.height = Math.max(getHeight(x.left),getHeight(x.right));
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
         return x;
     }
+
     public V remove(K key) {
         Node node = getNode(root, key);
         if (node != null) {
@@ -87,48 +98,55 @@ public class AVLTree<K extends Comparable<K>, V>{
         return minimum(node.left);
     }
 
-    private Node removeMin(Node node) {
-        if (node.left == null) {
-            Node rightNode = node.right;
-            node.right = null;
-            size--;
-            return rightNode;
-        }
-        node.left = removeMin(node.left);
-        return node;
-    }
-
     private Node remove(Node node, K key) {
         if (node == null) return null;
+        Node retNode;
         if (key.compareTo(node.key) > 0) {
             node.right = remove(node.right, key);
-            return node;
+            retNode = node;
         } else if (key.compareTo(node.key) < 0) {
             node.left = remove(node.left, key);
-            return node;
+            retNode = node;
         } else {
             if (node.left == null) {
                 Node rightNode = node.right;
                 node.right = null;
                 size--;
-                return rightNode;
-            }
-
-            if (node.right == null) {
+                retNode = rightNode;
+            } else if (node.right == null) {
                 Node leftNode = node.left;
                 node.left = null;
                 size--;
-                return leftNode;
+                retNode = leftNode;
+            } else {
+                Node successor = minimum(node.right);
+                successor.right = remove(node.right, successor.key);
+                successor.left = node.left;
+
+                node.left = node.right = null;
+
+                retNode = successor;
             }
-
-            Node successor = minimum(node.right);
-            successor.right = removeMin(node.right);
-            successor.left = node.left;
-
-            node.left = node.right = null;
-
-            return successor;
         }
+        if(retNode==null)return null;
+
+        retNode.height = 1 + Math.max(getHeight(retNode.left), getHeight(retNode.right));
+        int balanceFactor = getBalanceFactor(retNode);
+        if (balanceFactor > 1 && getBalanceFactor(retNode.left) >= 0) {
+            return rightRotate(retNode);
+        }
+        if (balanceFactor < -1 && getBalanceFactor(retNode.right) <= 0) {
+            return leftRotate(retNode);
+        }
+        if (balanceFactor > 1 && getBalanceFactor(retNode.left) < 0) {
+            retNode.left = leftRotate(retNode.left);
+            return rightRotate(retNode);
+        }
+        if (balanceFactor < -1 && getBalanceFactor(retNode.right) > 0) {
+            retNode.right = rightRotate(retNode.right);
+            return leftRotate(retNode);
+        }
+        return retNode;
     }
 
     public boolean contains(K key) {
@@ -153,40 +171,47 @@ public class AVLTree<K extends Comparable<K>, V>{
     public boolean isEmpty() {
         return size == 0;
     }
-    public boolean isBST(){
+
+    public boolean isBST() {
         ArrayList<K> keys = new ArrayList<>();
-        inOrder(root,keys);
+        inOrder(root, keys);
         for (int i = 1; i < keys.size(); i++) {
-            if(keys.get(i-1).compareTo(keys.get(i)) > 0)
+            if (keys.get(i - 1).compareTo(keys.get(i)) > 0)
                 return false;
         }
         return true;
     }
-    private void inOrder(Node node,ArrayList<K> keys){
-        if(node == null)return;
-        inOrder(node.left,keys);
+
+    private void inOrder(Node node, ArrayList<K> keys) {
+        if (node == null) return;
+        inOrder(node.left, keys);
         keys.add(node.key);
-        inOrder(node.right,keys);
+        inOrder(node.right, keys);
     }
-    public boolean isBalanced(){
+
+    public boolean isBalanced() {
         return isBalanced(root);
     }
-    private boolean isBalanced(Node node){
-        if(node == null)return true;
+
+    private boolean isBalanced(Node node) {
+        if (node == null) return true;
         int balanceFactor = getBalanceFactor(node);
-        if(Math.abs(balanceFactor) > 1){
+        if (Math.abs(balanceFactor) > 1) {
             return false;
         }
         return isBalanced(node.left) && isBalanced(node.right);
     }
-    private int getHeight(Node node){
-        if(node == null)return 0;
+
+    private int getHeight(Node node) {
+        if (node == null) return 0;
         return node.height;
     }
-    private int getBalanceFactor(Node node){
-        if(node == null)return 0;
+
+    private int getBalanceFactor(Node node) {
+        if (node == null) return 0;
         return getHeight(node.left) - getHeight(node.right);
     }
+
     private Node getNode(Node node, K key) {
         if (node == null) return null;
         if (key.compareTo(node.key) == 0) return node;
@@ -210,8 +235,15 @@ public class AVLTree<K extends Comparable<K>, V>{
             }
             System.out.println("Total different words: " + map.getSize());
             System.out.println("Frequency of PRIDE: " + map.get("prejudice"));
-            System.out.println("is BST："+map.isBST());
-            System.out.println("is Balanced："+map.isBalanced());
+            System.out.println("is BST：" + map.isBST());
+            System.out.println("is Balanced：" + map.isBalanced());
+
+            for (String word:words) {
+                map.remove(word);
+                if(!map.isBST()||!map.isBalanced()){
+                    throw new RuntimeException("Error");
+                }
+            }
         }
     }
 
