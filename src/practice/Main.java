334211532059792393eaae3463b1d8a036d2d633
LelandYan <2721599586@qq.com>
2019-05-11 15:21:03 +0800

    分支结构

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -1,26 +1,26 @@
 package practice;
 
-
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int need = sc.nextInt();
-        int[] arr = new int[6];
-        int cnt = 0;
-        int n,m;
-        int minValue=Integer.MAX_VALUE;
-        while(cnt != 6){
-            arr[cnt++] = sc.nextInt();
-            arr[cnt++] = sc.nextInt();
+        int day = sc.nextInt();//3
+        long days = sc.nextInt();//10
+        long[] week = new long[7];
+        int cnt = 1;
+        for(int i=day-1;cnt<=days;i++,cnt++){
+            if(i>6){
+                i-=7;
+            }
+            week[i]++;
         }
-        for(int i = 0; i < 6; i+=2){
-            n = need/arr[i];
-            if((need-n*arr[i])!=0)n+=1;
-            m = n*arr[i+1];
-            if(m<minValue)minValue=m;
+        long total=0;
+        for(int i = 0; i < 7; i++){
+            if(i<5){
+                total +=(week[i]*250);
+            }
         }
-        System.out.println(minValue);
+        System.out.println(total);
     }
 }
